ce54de4b43b165d8a8c8dc5d30b153908e391907
upnt <upnt.github@gmail.com>
2022-03-10 21:03:13 +0900

    fix docker compose

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,7 +1,2 @@
-CREATE DATABASE mh_database;
-use mh_database;
-
-CREATE TABLE armers (
-    series   VARCHAR(20) NOT NULL PRIMARY KEY,
-    position VARCHAR(8)  NOT NULL PRIMARY KEY
-);
+CREATE DATABASE mh;
+use mh;
