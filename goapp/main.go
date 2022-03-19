7a49d2ff8abd50413d0262abcbcb8a4fa6fc9c10
unknown <upnt>
2022-03-19 23:34:19 +0900

    enable to get all monster's names

--- goapp/main.go
+++ goapp/main.go
@@ -2,27 +2,57 @@ package main
 
 import (
     "fmt"
-    "database/sql"
+    "github.com/gin-gonic/gin"
+    "net/http"
+    "github.com/jinzhu/gorm"
     _ "github.com/go-sql-driver/mysql"
 )
 
+type Monster struct {
+    ID      string `json:"id"`
+    NameEng string `json:"name_eng"`
+    NameJpn string `json:"name_jpn"`
+}
 
 func main() {
-    dbconf  := "root:rsQlQy4S12YY0@tcp(localhost:3306)/mh_database?charset=utf8mb4"
-    db, err := sql.Open("mysql", dbconf)
+    router := gin.Default()
 
-    defer db.Close()
-
+    db, err := connectDB()
     if err != nil {
-        fmt.Println(err.Error())
-    }
-
-    err = db.Ping()
-
-    if err != nil {
-        fmt.Println(err.Error())
-        return
+        panic(err.Error())
     } else {
-        fmt.Println("connect: connection success")
+        fmt.Println("Connected")
     }
+
+    monsters := []*Monster{}
+
+    tx := db.Table("monster").Begin()
+    err = tx.Find(&monsters).Commit().Error
+
+    if err != nil {
+        panic(err.Error())
+    } else {
+        fmt.Println("get all monsters")
+    }
+
+    router.GET("/", func(ctx *gin.Context){
+        ctx.JSON(http.StatusOK, monsters)
+    })
+
+    router.Run(":8080")
+}
+
+func connectDB() (database *gorm.DB, err error) {
+    DBMS     := "mysql"
+
+    USER     := "root"
+    PASSWORD := "rsQlQy4S12YY0"
+    PROTOCOL := "tcp(mysql:3306)"
+    DBNAME   := "mh"
+
+    CONNECT := USER + ":" + PASSWORD
+    CONNECT += "@" + PROTOCOL + "/" + DBNAME
+    CONNECT += "?charset=utf8&&parseTime=true&loc=Asia%2fTokyo"
+
+    return gorm.Open(DBMS, CONNECT)
 }
